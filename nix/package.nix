5570d2cc6468ced4df33df7f3f840e6b2335dfd6
Pierrot LC <pierrotlc@proton.me>
2024-07-19 23:38:43 +0200

    feat: adding bookmarks, better settings, another extension

--- nix/package.nix
+++ nix/package.nix
@@ -8,22 +8,81 @@
     libName = "librewolf";
     nativeMessagingHosts = with pkgs; [bukubrow];
     extraPrefs = ''
-      defaultPref("accessibility.force_disabled", 1);
-      defaultPref("browser.aboutConfig.showWarning", false);
-      defaultPref("browser.shell.checkDefaultBrowser", false);
-      defaultPref("browser.translations.neverTranslateLanguages", "fr");
-      defaultPref("extensions.autoDisableScopes", 0);
-      defaultPref("extensions.autoDisableScopes", 0);
-      defaultPref("extensions.install_origins.enabled", true);
-      defaultPref("general.autoScroll", true);
-      defaultPref("gfx.canvas.accelerated", true);
-      defaultPref("gfx.webrender.enabled", true);
-      defaultPref("middlemouse.paste", false);
-      defaultPref("privacy.clearOnShutdown.cache", false);
-      defaultPref("privacy.clearOnShutdown.cookies", false);
-      defaultPref("webgl.disabled", false);
+      pref("accessibility.force_disabled", 1);
+      pref("browser.aboutConfig.showWarning", false);
+      pref("browser.bookmarks.addedImportButton", false);
+      pref("browser.migrate.bookmarks-file.enabled", false);
+      pref("browser.shell.checkDefaultBrowser", false);
+      pref("browser.toolbars.bookmarks.visibility", "newtab");
+      pref("browser.translations.neverTranslateLanguages", "fr");
+      pref("extensions.autoDisableScopes", 0);
+      pref("extensions.autoDisableScopes", 0);
+      pref("extensions.install_origins.enabled", true);
+      pref("general.autoScroll", true);
+      pref("gfx.canvas.accelerated", true);
+      pref("gfx.webrender.enabled", true);
+      pref("middlemouse.paste", false);
+      pref("privacy.clearOnShutdown.cache", false);
+      pref("privacy.clearOnShutdown.cookies", false);
+      pref("privacy.resistFingerprinting.exemptedDomains", "*.twitch.tv");
+      pref("webgl.disabled", false);
     '';
     extraPolicies = {
+      # Documentation about policies options can be found at `about:policies#documentation`.
+      # You can also have a look here: https://github.com/mozilla/policy-templates/.
+      Bookmarks = [
+        {
+          "Title" = "";
+          "URL" = "https://youtube.com";
+          "Placement" = "toolbar";
+        }
+        {
+          "Title" = "";
+          "URL" = "https://music.youtube.com";
+          "Placement" = "toolbar";
+        }
+        {
+          "Title" = "";
+          "URL" = "https://www.twitch.tv/";
+          "Placement" = "toolbar";
+        }
+        {
+          "Title" = "";
+          "URL" = "https://x.com";
+          "Placement" = "toolbar";
+        }
+        {
+          "Title" = "";
+          "URL" = "https://discord.com/channels/@me";
+          "Placement" = "toolbar";
+        }
+        {
+          "Title" = "";
+          "URL" = "https://messenger.com";
+          "Placement" = "toolbar";
+        }
+        {
+          "Title" = "";
+          "URL" = "https://web.whatsapp.com/";
+          "Placement" = "toolbar";
+        }
+        {
+          "Title" = "";
+          "URL" = "https://github.com/";
+          "Placement" = "toolbar";
+        }
+        {
+          "Title" = "";
+          "URL" = "https://wandb.ai";
+          "Placement" = "toolbar";
+        }
+        {
+          "Title" = "";
+          "URL" = "https://search.nixos.org/packages";
+          "Placement" = "toolbar";
+        }
+      ];
+
       # Extensions are obtained thanks to the guide here: https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265.
       # Check `about:support` for extension/add-on ID strings. Then find the
       # installation url by donwloading the extension file (instead of
@@ -52,6 +111,10 @@
         };
         "{e7476172-097c-4b77-b56e-f56a894adca9}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4188531/minimaltwitter-latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4321653/privacy_badger17-latest.xpi";
           installation_mode = "force_installed";
         };
       };
