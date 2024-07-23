d942c5b11e1a03d6bfee90e183fbaddc4e7608a1
Pierrot LC <pierrotlc@proton.me>
2024-07-23 20:14:45 +0200

    feat(extensions): adding refined github

--- nix/package.nix
+++ nix/package.nix
@@ -112,32 +112,36 @@
       # installing it directly). Always install the latest version of the
       # extensions by using the "latest" tag in the download url.
       ExtensionSettings = {
-        "zotero@chnm.gmu.edu" = {
-          install_url = "https://www.zotero.org/download/connector/dl?browser=firefox";
-          installation_mode = "force_installed";
-        };
-        "bukubrow@samhh.com" = {
+        "bukubrow@samhh.com" = {  # Bukubrow
           install_url = "https://addons.mozilla.org/firefox/downloads/file/3769984/bukubrow-latest.xpi";
           installation_mode = "force_installed";
         };
-        "CanvasBlocker@kkapsner.de" = {
+        "CanvasBlocker@kkapsner.de" = {  # Canvas Blocker
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4262820/canvasblocker-latest.xpi";
           installation_mode = "force_installed";
         };
-        "{44df5123-f715-9146-bfaa-c6e8d4461d44}" = {
+        "{44df5123-f715-9146-bfaa-c6e8d4461d44}" = {  # Fakespot
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4312344/fakespot_fake_reviews_amazon-latest.xpi";
           installation_mode = "force_installed";
         };
-        "{15bdb1ce-fa9d-4a00-b859-66c214263ac0}" = {
+        "{15bdb1ce-fa9d-4a00-b859-66c214263ac0}" = {  # Get RSS Feed URL
           install_url = "https://addons.mozilla.org/firefox/downloads/file/3990496/get_rss_feed_url-latest.xpi";
           installation_mode = "force_installed";
         };
-        "{e7476172-097c-4b77-b56e-f56a894adca9}" = {
+        "{e7476172-097c-4b77-b56e-f56a894adca9}" = {  # Minimal Twitter
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4188531/minimaltwitter-latest.xpi";
           installation_mode = "force_installed";
         };
-        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {  # Privacy Badger
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4321653/privacy_badger17-latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}" = {  # Refined GitHub
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4317387/refined_github-latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "zotero@chnm.gmu.edu" = {  # Zotero Connector
+          install_url = "https://www.zotero.org/download/connector/dl?browser=firefox";
           installation_mode = "force_installed";
         };
       };
