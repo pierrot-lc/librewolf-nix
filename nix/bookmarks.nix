a900b8e48d7deca86874d5e7f6a6273ee764d1e7
Pierrot LC <pierrotlc@proton.me>
2024-07-24 23:56:48 +0200

    feat: bookmarks favicons

---
+++ nix/bookmarks.nix
@@ -0,0 +1,83 @@
+# List of bookmarks, either in the toolbar or in the menu.
+# See the expected format here: https://github.com/mozilla/policy-templates/blob/master/linux/policies.json#L33.
+# The favicon are provided by a public API service, see https://stackoverflow.com/questions/46369862/get-the-favicon-of-a-url-and-display-it-firefox-web-ext.
+[
+  {
+    "Title" = "";
+    "URL" = "https://youtube.com";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/youtube.com.ico";
+    "Placement" = "toolbar";
+  }
+  {
+    "Title" = "";
+    "URL" = "https://music.youtube.com";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/music.youtube.com.ico";
+    "Placement" = "toolbar";
+  }
+  {
+    "Title" = "";
+    "URL" = "https://www.twitch.tv";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/twitch.tv.ico";
+    "Placement" = "toolbar";
+  }
+  {
+    "Title" = "";
+    "URL" = "https://x.com";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/x.com.ico";
+    "Placement" = "toolbar";
+  }
+  {
+    "Title" = "";
+    "URL" = "https://discord.com/channels/@me";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/discord.com.ico";
+    "Placement" = "toolbar";
+  }
+  {
+    "Title" = "";
+    "URL" = "https://messenger.com";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/messenger.com.ico";
+    "Placement" = "toolbar";
+  }
+  {
+    "Title" = "";
+    "URL" = "https://web.whatsapp.com";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/web.whatsapp.com.ico";
+    "Placement" = "toolbar";
+  }
+  {
+    "Title" = "";
+    "URL" = "https://github.com";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/github.com.ico";
+    "Placement" = "toolbar";
+  }
+  {
+    "Title" = "";
+    "URL" = "https://wandb.ai";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/wandb.ai.ico";
+    "Placement" = "toolbar";
+  }
+  {
+    "Title" = "";
+    "URL" = "https://search.nixos.org/packages";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/search.nixos.org.ico";
+    "Placement" = "toolbar";
+  }
+  {
+    "Title" = "Nixpkgs";
+    "URL" = "https://github.com/NixOS/nixpkgs";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/github.com.ico";
+    "Placement" = "menu";
+  }
+  {
+    "Title" = "";
+    "URL" = "https://rss.flus.fr";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/rss.flus.fr.ico";
+    "Placement" = "menu";
+  }
+  {
+    "Title" = "IceDrive";
+    "URL" = "https://icedrive.net/";
+    "Favicon" = "https://icons.duckduckgo.com/ip3/icedrive.net.ico";
+    "Placement" = "menu";
+  }
+]
--- nix/package.nix
+++ nix/package.nix
@@ -1,107 +1,44 @@
 {
   pkgs,
   lib,
-}: {
+}: let
+  bookmarks = import ./bookmarks.nix;
+in {
   librewolf-pkg = pkgs.wrapFirefox pkgs.librewolf-unwrapped {
     inherit (pkgs.librewolf-unwrapped) extraPrefsFiles extraPoliciesFiles;
     wmClass = "LibreWolf";
     libName = "librewolf";
     nativeMessagingHosts = with pkgs; [bukubrow];
     extraPrefs = /* javascript */ ''
-      pref("accessibility.force_disabled", 1);
-      pref("browser.aboutConfig.showWarning", false);
-      pref("browser.bookmarks.addedImportButton", false);
-      pref("browser.migrate.bookmarks-file.enabled", false);
-      pref("browser.shell.checkDefaultBrowser", false);
-      pref("browser.toolbars.bookmarks.visibility", "newtab");
-      pref("browser.translations.neverTranslateLanguages", "fr");
-      pref("extensions.autoDisableScopes", 0);
-      pref("extensions.install_origins.enabled", true);
-      pref("general.autoScroll", true);
-      pref("gfx.canvas.accelerated", true);
-      pref("gfx.webrender.enabled", true);
-      pref("middlemouse.paste", false);
-      pref("webgl.disabled", false);
+        pref("accessibility.force_disabled", 1);
+        pref("browser.aboutConfig.showWarning", false);
+        pref("browser.bookmarks.addedImportButton", false);
+        pref("browser.migrate.bookmarks-file.enabled", false);
+        pref("browser.shell.checkDefaultBrowser", false);
+        pref("browser.toolbars.bookmarks.visibility", "newtab");
+        pref("browser.translations.neverTranslateLanguages", "fr");
+        pref("extensions.autoDisableScopes", 0);
+        pref("extensions.install_origins.enabled", true);
+        pref("general.autoScroll", true);
+        pref("gfx.canvas.accelerated", true);
+        pref("gfx.webrender.enabled", true);
+        pref("middlemouse.paste", false);
+        pref("webgl.disabled", false);
 
-      // Privacy relaxation.
-      pref("privacy.clearOnShutdown.cache", false);
-      pref("privacy.clearOnShutdown.cookies", false);
-      pref("privacy.resistFingerprinting.exemptedDomains", "*.twitch.tv,*.discord.com,*.x.com");
-    '';
+        // Privacy relaxation.
+        pref("privacy.clearOnShutdown.cache", false);
+        pref("privacy.clearOnShutdown.cookies", false);
+        pref("privacy.resistFingerprinting.exemptedDomains", "*.discord.com,*.twitch.tv,*.wandb.ai,*.x.com");
+      '';
     extraPolicies = {
       # Documentation about policies options can be found at `about:policies#documentation`.
       # You can also have a look here: https://github.com/mozilla/policy-templates/.
-      Bookmarks = [
-        {
-          "Title" = "";
-          "URL" = "https://youtube.com";
-          "Placement" = "toolbar";
-        }
-        {
-          "Title" = "";
-          "URL" = "https://music.youtube.com";
-          "Placement" = "toolbar";
-        }
-        {
-          "Title" = "";
-          "URL" = "https://www.twitch.tv";
-          "Placement" = "toolbar";
-        }
-        {
-          "Title" = "";
-          "URL" = "https://x.com";
-          "Placement" = "toolbar";
-        }
-        {
-          "Title" = "";
-          "URL" = "https://discord.com/channels/@me";
-          "Placement" = "toolbar";
-        }
-        {
-          "Title" = "";
-          "URL" = "https://messenger.com";
-          "Placement" = "toolbar";
-        }
-        {
-          "Title" = "";
-          "URL" = "https://web.whatsapp.com";
-          "Placement" = "toolbar";
-        }
-        {
-          "Title" = "";
-          "URL" = "https://github.com";
-          "Placement" = "toolbar";
-        }
-        {
-          "Title" = "";
-          "URL" = "https://wandb.ai";
-          "Placement" = "toolbar";
-        }
-        {
-          "Title" = "";
-          "URL" = "https://search.nixos.org/packages";
-          "Placement" = "toolbar";
-        }
-        {
-          "Title" = "Nixpkgs";
-          "URL" = "https://github.com/NixOS/nixpkgs";
-          "Placement" = "menu";
-        }
-        {
-          "Title" = "";
-          "URL" = "https://rss.flus.fr";
-          "Placement" = "menu";
-        }
-        {
-          "Title" = "IceDrive";
-          "URL" = "https://icedrive.net/";
-          "Placement" = "menu";
-        }
-      ];
+      Bookmarks = bookmarks;
 
       Cookies = {
         Allow = [
           "https://discord.com"
+          "https://wandb.ai/"
           "https://www.twitch.tv/"
         ];
       };
@@ -112,35 +49,43 @@
       # installing it directly). Always install the latest version of the
       # extensions by using the "latest" tag in the download url.
       ExtensionSettings = {
-        "bukubrow@samhh.com" = {  # Bukubrow
+        "bukubrow@samhh.com" = {
+          # Bukubrow
           install_url = "https://addons.mozilla.org/firefox/downloads/file/3769984/bukubrow-latest.xpi";
           installation_mode = "force_installed";
         };
-        "CanvasBlocker@kkapsner.de" = {  # Canvas Blocker
+        "CanvasBlocker@kkapsner.de" = {
+          # Canvas Blocker
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4262820/canvasblocker-latest.xpi";
           installation_mode = "force_installed";
         };
-        "{44df5123-f715-9146-bfaa-c6e8d4461d44}" = {  # Fakespot
+        "{44df5123-f715-9146-bfaa-c6e8d4461d44}" = {
+          # Fakespot
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4312344/fakespot_fake_reviews_amazon-latest.xpi";
           installation_mode = "force_installed";
         };
-        "{15bdb1ce-fa9d-4a00-b859-66c214263ac0}" = {  # Get RSS Feed URL
+        "{15bdb1ce-fa9d-4a00-b859-66c214263ac0}" = {
+          # Get RSS Feed URL
           install_url = "https://addons.mozilla.org/firefox/downloads/file/3990496/get_rss_feed_url-latest.xpi";
           installation_mode = "force_installed";
         };
-        "{e7476172-097c-4b77-b56e-f56a894adca9}" = {  # Minimal Twitter
+        "{e7476172-097c-4b77-b56e-f56a894adca9}" = {
+          # Minimal Twitter
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4188531/minimaltwitter-latest.xpi";
           installation_mode = "force_installed";
         };
-        "jid1-MnnxcxisBPnSXQ@jetpack" = {  # Privacy Badger
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          # Privacy Badger
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4321653/privacy_badger17-latest.xpi";
           installation_mode = "force_installed";
         };
-        "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}" = {  # Refined GitHub
+        "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}" = {
+          # Refined GitHub
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4317387/refined_github-latest.xpi";
           installation_mode = "force_installed";
         };
-        "zotero@chnm.gmu.edu" = {  # Zotero Connector
+        "zotero@chnm.gmu.edu" = {
+          # Zotero Connector
           install_url = "https://www.zotero.org/download/connector/dl?browser=firefox";
           installation_mode = "force_installed";
         };
